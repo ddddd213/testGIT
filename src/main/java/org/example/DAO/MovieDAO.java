915accf153e77005f5cc6d9e4e7bd8a97d9f35f8
Hieule02 <hieulehd02@gmail.com>
2024-03-31 22:52:57 +0700

    done function getAllMovies

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -3,4 +3,7 @@ package org.example.DAO;
 import org.example.entities.Movie;
 
 public class MovieDAO implements BaseDAO<Movie> {
+    public boolean create(Movie movie) {
+        return false;
+    }
 }
