e363cd2d35a637b694eda3f0c26d09c673ecff48
Hieule02 <hieulehd02@gmail.com>
2024-03-31 23:26:03 +0700

    done function getMoviesByCompany

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -1,6 +1,25 @@
 package org.example.DAO;
 
 import org.example.entities.Movie;
+import org.example.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class MovieDAO implements BaseDAO<Movie> {
+    public List<Movie> getMoviesByCompany(String company) {
+        Session session = null;
+        try {
+            session = HibernateUtils.getInstance().openSession();
+            String hql = "FROM Movie m WHERE m.movieProductionCompany = :company";
+            Query<Movie> query = session.createQuery(hql, Movie.class);
+            query.setParameter("company", company);
+            return query.list();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
