696d3fa796be91d23d37bce6cb973751b59edea0
tuyethg2001 <tuyethg2792001@gmail.com>
2024-03-31 23:58:27 +0700

    fix movie pagination merge

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -9,8 +9,6 @@ import org.hibernate.Session;
 import org.hibernate.mapping.List;
 import org.hibernate.query.Query;
 
-
-
 public class MovieDAO implements BaseDAO<Movie> {
 
     public boolean ifExistedByNameVn(String nameVn){
@@ -44,4 +42,27 @@ public class MovieDAO implements BaseDAO<Movie> {
         }
     }
 
+    /**
+     * Retrieves a paginated list of entities from the database.
+     * @param pageNumber - The page number of the results to retrieve
+     * @param pageSize - The maximum number of entities per page
+     * @return A list of entities retrieved from the database
+     */
+    public List<Movie> getMovies(int pageNumber, int pageSize) {
+        try (Session session = HibernateUtils.getInstance().openSession()) {
+            // Create CriteriaBuilder
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Movie> criteriaQuery = builder.createQuery(Movie.class);
+            Root<Movie> root = criteriaQuery.from(Movie.class);
+            criteriaQuery.select(root).orderBy(builder.asc(root.get("id")));
+
+            // Execute query with pagination
+            Query<Movie> query = session.createQuery(criteriaQuery);
+            query.setFirstResult((pageNumber - 1) * pageSize);
+            query.setMaxResults(pageSize);
+            List<Movie> movies = query.getResultList();
+
+            return movies;
+        }
+    }
 }
