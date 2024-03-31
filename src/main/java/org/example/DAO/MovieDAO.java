ce1ad0174c1de2e15269f2a1a852ab00862fd847
HuynhVT2 <66322987+Huynhcoimttngh21@users.noreply.github.com>
2024-03-31 23:58:12 +0700

    Revert "update-findMovieByName"

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -6,7 +6,6 @@ import jakarta.persistence.criteria.Root;
 import org.example.entities.Movie;
 import org.example.utils.HibernateUtils;
 import org.hibernate.Session;
-import org.hibernate.mapping.List;
 import org.hibernate.query.Query;
 
 
@@ -28,20 +27,4 @@ public class MovieDAO implements BaseDAO<Movie> {
 
         }
     }
-
-    public List<Movie> findMovieByName(String movieName) {
-        Session session = null;
-        try {
-            session = HibernateUtils.getInstance().openSession();
-            String hql = "FROM Movie m WHERE m.name = :movieName";
-            Query<Movie> query = session.createQuery(hql, Movie.class);
-            query.setParameter("movieName", movieName);
-            return query.list();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-    }
-
 }
