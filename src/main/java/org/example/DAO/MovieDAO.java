db233782dc1db974350b90ee16b89783c5998fc3
Nguyễn Quang Tùng <nqt2612001@gmail.com>
2024-03-31 23:47:42 +0700

    Add DAO Function

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -1,6 +1,7 @@
 package org.example.DAO;
 
 import org.example.entities.Movie;
-
 public class MovieDAO implements BaseDAO<Movie> {
+    public MovieDAO() {
+    }
 }
--- src/main/java/org/example/DAO/MovieTypeDAO.java
+++ src/main/java/org/example/DAO/MovieTypeDAO.java
@@ -1,6 +1,27 @@
 package org.example.DAO;
 
 import org.example.entities.MovieType;
+import org.example.utils.HibernateUtils;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class MovieTypeDAO implements BaseDAO<MovieType>{
+    public MovieTypeDAO() {
+    }
+
+    public List<MovieType> getMovieTypeByTypeId(int typeId) {
+        Session session = null;
+        try {
+            session = HibernateUtils.getInstance().openSession();
+            String hql = "from MovieType where id = :typeId";
+            return session.createQuery(hql, MovieType.class)
+                    .setParameter("typeId", typeId)
+                    .list();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
