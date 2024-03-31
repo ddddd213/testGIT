7beb4d80472b9d11ffce5a4c120149aaba6839dd
HuynhVT2 <66322987+Huynhcoimttngh21@users.noreply.github.com>
2024-04-01 00:32:44 +0700

    Update Type.java

--- src/main/java/org/example/entities/Type.java
+++ src/main/java/org/example/entities/Type.java
@@ -2,7 +2,7 @@ package org.example.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
@@ -17,10 +17,11 @@ public class Type {
     @Column(name = "TYPE_ID", unique = true, nullable = false)
     private Integer id;
     @Column(name = "TYPE_NAME", unique = true, nullable = false)
-    @NonNull
+    @NotBlank(message = "Type name cannot be blank")
     private String typeName;
     @Column(name = "TYPE_DESCRIPTION", nullable = false)
-    @NonNull
+    @NotBlank(message = "Description is required")
+    @Size(max = 255, message = "Description must be at most 255 characters")
     private String description;
     @OneToMany(mappedBy = "type", fetch = FetchType.LAZY)
     private List<MovieType> movieTypeList;
