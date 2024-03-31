7e7db39b611bb7e6b77a6402601987fa0f7409a7
GiangNVT5 <nkhoa5949@gmail.com>
2024-04-01 00:04:45 +0700

    Update MovieService.java

--- src/main/java/org/example/services/MovieService.java
+++ src/main/java/org/example/services/MovieService.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class MovieService {
 
-    public void AddComment() {
-        ArrayList<String> arrayList = new ArrayList<>();
-        // Add strings to the ArrayList
-        arrayList.add("Hello");
-        arrayList.add("World");
-        arrayList.add("!");
-
-        // Print the ArrayList
-        System.out.println(arrayList);
+//    public void AddComment() {
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        // Add strings to the ArrayList
+//        arrayList.add("Hello");
+//        arrayList.add("World");
+//        arrayList.add("!");
+//
+//        // Print the ArrayList
+//        System.out.println(arrayList);
     }
 }
