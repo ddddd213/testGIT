9d76ec73355da65aff694032e0a9e64ec9d9e064
quyen quyen <quyenvnk63@gmail.com>
2024-03-31 23:59:08 +0700

    create-find-movie-by-name-service

--- src/main/java/org/example/services/MovieService.java
+++ src/main/java/org/example/services/MovieService.java
@@ -1,4 +1,17 @@
 package org.example.services;
 
+import org.example.DAO.MovieDAO;
+import org.example.entities.Movie;
+
 public class MovieService {
+    private MovieDAO movieDAO;
+    MovieService(){
+        movieDAO = new MovieDAO();
+    }
+    public void showMovieByName(String name){
+        List<Movie> movieList = movieDAO.findMovieByName(name);
+        for (Movie movie: movieList) {
+            System.out.println(movie.getMovieNameVn());
+        }
+    }
 }
