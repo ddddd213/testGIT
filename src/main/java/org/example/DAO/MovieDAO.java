87e4f0dc516f8f9b9ab50f4015ae4a86dd64e2ed
Vũ Trung Hiếu <vutrunghieu2252000@gmail.com>
2024-03-31 23:40:04 +0700

    create MovieDao ifExistedByNameVn done

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -1,6 +1,25 @@
 package org.example.DAO;
 
 import org.example.entities.Movie;
+import org.example.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 
 public class MovieDAO implements BaseDAO<Movie> {
+
+    public boolean ifExistedByNameVn(String nameVn){
+        Session session = null;
+        try{
+            session = HibernateUtils.getInstance().openSession();
+            Query query = session.createNativeQuery("SELECT * FROM Movie m WHERE m.name_vn= :nameVn ", Movie.class);
+            query.setParameter("nameEng", nameVn);
+            Movie movie = (Movie) query.getSingleResult();
+            return (movie!=null);
+        } finally {
+            if(session!=null){
+                session.close();
+            }
+        }
+    }
 }
