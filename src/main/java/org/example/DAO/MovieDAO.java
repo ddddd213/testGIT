7748defc803b9e0627dee8c626f36e9c51a3f118
quyen quyen <quyenvnk63@gmail.com>
2024-03-31 23:47:42 +0700

    update-findMovieByName

--- src/main/java/org/example/DAO/MovieDAO.java
+++ src/main/java/org/example/DAO/MovieDAO.java
@@ -6,6 +6,7 @@ import jakarta.persistence.criteria.Root;
 import org.example.entities.Movie;
 import org.example.utils.HibernateUtils;
 import org.hibernate.Session;
+import org.hibernate.mapping.List;
 import org.hibernate.query.Query;
 
 
@@ -27,4 +28,20 @@ public class MovieDAO implements BaseDAO<Movie> {
 
         }
     }
+
+    public List<Movie> findMovieByName(String movieName) {
+        Session session = null;
+        try {
+            session = HibernateUtils.getInstance().openSession();
+            String hql = "FROM Movie m WHERE m.name = :movieName";
+            Query<Movie> query = session.createQuery(hql, Movie.class);
+            query.setParameter("movieName", movieName);
+            return query.list();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
 }
